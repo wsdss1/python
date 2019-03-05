b9fa4846fe4c38d18dc74a0da12d2625cea9581e
kirill <white_bki@mail.ru>
2019-03-05 11:34:53 +0300

    working relise

--- create_folders.py
+++ create_folders.py
@@ -17,7 +17,9 @@ ftp = FTP('10.200.12.228', 'ftpuser', '12345')
 waveform_archive_t = ['to','t01','t02','t03','t04','t05','t06','t07','t08','t09','t0A','t0B','t0C','t0D','t0E','t0F']
 waveform_archive_d = ['do','d01','d02','d03','d04','d05','d06','d07','d08','d09','d0A','d0B','d0C','d0D','d0E','d0F']
 
-temp_folder = './temp_folder'
+temp_folder = "temp_folder"
+current_dir = os.path.dirname(os.path.realpath(__file__))
+NEW_PATH = 'temp_folder_sended'
 
 # создаём logger
 logger = logging.getLogger(__name__)
@@ -51,60 +53,43 @@ def rnd_file_create():
     archives = [waveform_archive_t, waveform_archive_d]
     used_archive = random.choice(archives)
     # generate a random UUID
-    filename = f"{uuid.uuid4()}"
+    filename = f'{uuid.uuid4()}'
+    create_dir(temp_folder)
     for i in used_archive:
-        full_filename = f"./temp_folder/{filename}.{i}"
-        newfile = open(full_filename, "wb")
+        full_filename = f'{current_dir}/temp_folder/{filename}.{i}'
+        newfile = open(full_filename, 'wb')
         size = random.randint(100000, 9999999) # in bytes
         newfile.seek(size)
-        newfile.write(b"\0")
+        newfile.write(b'\0')
         logger.info(f'{sys._getframe().f_code.co_name}  {full_filename}  size= {size} bytes')
         newfile.close()
 
 # upload file on ftp
 def upload_ftp(file):
-    func_name = sys._getframe().f_code.co_name
+    logger.info(f'{sys._getframe().f_code.co_name}  try connect to {HOST}:{PORT} ftp server')
     ftp.connect(HOST, PORT)
+    logger.info(f'{sys._getframe().f_code.co_name}  try login to ftp server')
     ftp.login(UNAME, PS)
     # Перейти в директорию
-    ftp.cwd('/')
-    myfile = open(src, 'rb')
-    ftp.storbinary(f'STOR {filename}', myfile)
-    ftp.storbinary(f'STOR {filename}.flag', myfile)
+    ftp.cwd('/home/ftpuser/')
+    logger.info(f'{sys._getframe().f_code.co_name}  read file from temp_folder')
+    myfile = open(f'{current_dir}/temp_folder/{file}', 'rb')
+    logger.info(f'{sys._getframe().f_code.co_name}  try stor file to ftp server')
+    ftp.storbinary(f'STOR {file}', myfile)
+    logger.info(f'{sys._getframe().f_code.co_name}  file sended to ftp server')
     myfile.close()
 
-'''
 def main():
-    file_list = os.listdir(temp_folder)
-    num_files = len(os.listdir(temp_folder))
-    logger.info(f'read {num_files} files in {temp_folder}')
+    file_list = os.listdir(f'./{temp_folder}')
+    num_files = len(os.listdir(f'./{temp_folder}'))
+    logger.info(f'{sys._getframe().f_code.co_name}  read {num_files} files in {temp_folder}')
     for file in file_list:
-        upload_ftp(file_name)
-        logger.info(f'try upload {file_name} to ftp server') 
-
-
-                print("пытаюсь загрузить файл")
-                upload_ftp(SUP, F1, file_name)
-                print("загрузил файл")
-                i = i+1
-                if i != num_files:
-                    print("еще не все передано")
-                    print(f"transfer {i} ({file_name}) from {num_files} files")
-                    if j <= (num_files - 1):
-                        delay_time = create_time_delay(time_sorted_list[j], time_sorted_list[j + 1])
-                        print(delay_time)
-                        j = j + 1
-                        time.sleep(delay_time)
-                        # move copied file to other path
-                        shutil.move(F1, NEW_PATH)
-                else:
-                    shutil.move(F1, NEW_PATH)
-                    print(f"\nWill be transfered {num_files} files. \n\t\tDone!")
-                    # move all files
-                    break
-'''
+        logger.info(f'{sys._getframe().f_code.co_name}  try upload {file} to ftp server') 
+        upload_ftp(file)
+        create_dir(NEW_PATH)
+        logger.info(f'{sys._getframe().f_code.co_name}  send file {file} to {NEW_PATH}') 
+        shutil.move(f'{current_dir}\\temp_folder\{file}', f'{current_dir}\{NEW_PATH}')
 
 if __name__ == '__main__':  
-    create_dir(temp_folder)
     rnd_file_create()
-
+    main()
