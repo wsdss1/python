d203bbb05ce159ac5fb38374baf87ce6b3fb5e51
kirill <white_bki@mail.ru>
2019-03-13 17:01:36 +0300

    add new

--- python learn/rw_visual.py
+++ python learn/rw_visual.py
@@ -5,9 +5,24 @@ while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
     rw = RandomWalk()
     rw.fill_walk()
+
+    # Назначение размера области просмотра.
+    plt.figure(figsize=(10, 6))
+    #plt.figure(dpi=128, figsize=(10, 6))
+
     point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, linewidth=0.1)
+    '''
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
     #plt.scatter(rw.x_values, rw.y_values, s=1)
+    # Выделение первой и последней точек.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=5)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',s=5)
+    '''
+    # Удаление осей.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
