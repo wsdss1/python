17554ccb4557c882bf91ae9ac064082739a2894b
kirill <white_bki@mail.ru>
2019-03-13 16:42:48 +0300

    случайного блуждания и нанесение

--- python learn/random_walk.py
+++ python learn/random_walk.py
@@ -1,7 +1,7 @@
 from random import choice
 class RandomWalk():
     """Класс для генерирования случайных блужданий."""
-    def __init__(self, num_points=10000):
+    def __init__(self, num_points=5000):
         """Инициализирует атрибуты блуждания."""
         self.num_points = num_points
         # Все блуждания начинаются с точки (0, 0).
--- python learn/rw_visual.py
+++ python learn/rw_visual.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# Построение случайного блуждания и нанесение точек на диаграмму.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=1)
-plt.show()
+while True:
+    # Построение случайного блуждания и нанесение точек на диаграмму.
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    #plt.scatter(rw.x_values, rw.y_values, s=1)
+    plt.show()
 
-keep_running = input("Make another walk? (y/n): ")
-if keep_running == 'n':
-    break
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
