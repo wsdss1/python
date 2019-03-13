2056e5dae1f84baad9a580505bbe6f5d88a6325e
kirill <white_bki@mail.ru>
2019-03-13 20:27:01 +0300

    g

--- python learn/random_walk.py
+++ python learn/random_walk.py
@@ -13,17 +13,20 @@ class RandomWalk():
         # Шаги генерируются до достижения нужной длины.
         while len(self.x_values) < self.num_points:
         # Определение направления и длины перемещения.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = get_step()
+            y_step = get_step()
             # Отклонение нулевых перемещений.
             if x_step == 0 and y_step == 0:
                 continue
             # Вычисление следующих значений x и y.
-            next_x = self.x_values[-1] + x_step
-            next_y = self.y_values[-1] + y_step
+                next_x = self.x_values[-1] + x_step
+                next_y = self.y_values[-1] + y_step
+
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+    
+    def get_step(self):
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        step = direction * distance
+        return step
