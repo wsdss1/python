b8176277a1770d8b8b41b9496bc45b811c3f1131
kirill <white_bki@mail.ru>
2019-03-14 14:05:10 +0300

    auto generate list

--- python learn/different_dice.py
+++ python learn/different_dice.py
@@ -1,28 +1,25 @@
-import pygal
 from die import Die
+import pygal
 
-# Создание кубика D6.
+# Создание кубиков D6 и D10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
-
-# Анализ результатов.
+# Analyze the results.
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
-#print(sum(frequencies))
-
+# Визуализация результатов.
 hist = pygal.Bar()
-hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = [i for i in range(2,13)]
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = [i for i in range(2,17)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
-
-hist.add('D6 + D^', frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add('D6 + D10', frequencies)
+hist.render_to_file('dice_visual.svg')
