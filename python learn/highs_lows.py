bebe2f740d283e289e9a3b23e90e37fdec047252
p <gf>
2019-06-26 12:49:39 +0300

    high and low temp

--- python learn/highs_lows.py
+++ python learn/highs_lows.py
@@ -3,26 +3,33 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Чтение дат и температурных максимумов из файла.
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
+        low = int(row[3])
+        lows.append(low)
 
+    # проверка результата
     print(highs)
     print(dates)
+    print(lows)
+
     # Нанесение данных на диаграмму.
     fig = plt.figure(dpi=64, figsize=(10, 6))
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
+
     # Форматирование диаграммы.
-    plt.title("Daily high temperatures, July 2014", fontsize=14)
+    plt.title("Daily high and low temperatures, July 2014", fontsize=14)
     plt.xlabel('', fontsize=12)
-    
+
     plt.ylabel("Temperature (F)", fontsize=12)
     plt.tick_params(axis='both', which='major', labelsize=14)
     plt.show()
