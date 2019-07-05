2b50877ead42554dc5d96838ba9ff42846c08018
p <gf>
2019-07-05 20:45:30 +0300

    add consist data

--- python learn/highs_lows.py
+++ python learn/highs_lows.py
@@ -3,18 +3,22 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Чтение дат и температурных максимумов из файла.
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        high = int(row[1])
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
     # проверка результата
     print(highs)
