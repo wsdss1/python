de03b0e6f60473a9bca397f02a03a1addd36ed2d
white <s>
2020-01-14 19:13:56 +0300

    add new file "check_ping host"

--- progress_bar_terminal.py
+++ progress_bar_terminal.py
@@ -5,7 +5,7 @@ from progress.bar import Bar
 
 mylist = [1,2,3,4,5,6,7,8]
 
-bar = Bar('SOME_INFO_NEAR_PROGRESS', max = len(mylist))
+bar = Bar(' SOME_INFO_NEAR_PROGRESS ', max = len(mylist))
 
 for item in mylist:
     bar.next()
